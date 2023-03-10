24e33bd8776a363400bf15ab2bd5601c68f8fca2
Omnia Gnena <omnia.gnena@vodafone.com>
2023-03-10 14:51:07 +0200

    clone project

---
+++ src/pages/search/search.tsx
@@ -0,0 +1,31 @@
+
+import "./search.css";
+
+function SearchPage(props: any) {
+    
+  return (
+    <div className="search-books">
+    <div className="search-books-bar">
+      <a
+        className="close-search"
+        href='/#'
+        onClick={() => props.setShowSearchpage(!props.showSearchPage)}
+      >
+        Close
+      </a>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title, author, or ISBN"
+        />
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid"></ol>
+    </div>
+  </div>
+
+  );
+}
+
+export default SearchPage;
